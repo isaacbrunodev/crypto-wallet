e80b1acd24c58ba6db3e86fa928365cf9be137f3
Isaac <isaacbruno1996@gmail.com>
2024-01-03 23:58:30 -0300

    testando as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  #get'/inicio',to:'welcome#index'
   resources :coins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 #get'/coins',to:'coins#index
